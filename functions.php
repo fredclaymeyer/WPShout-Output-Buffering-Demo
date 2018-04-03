a4743f866fe725af137eaaf8abc6ee78350b7c7b
Frederick Meyer <fred@pressupinc.com>
2018-04-03 15:38:08 -0500

    More inline commenting

--- functions.php
+++ functions.php
@@ -1,6 +1,29 @@
 <?php
 
-// Writing lots of raw HTML into a function that returns a value rather than echoing it
+add_shortcode( 'wpshout_quickview_post', 'wpshout_quickview_post' );
+function wpshout_quickview_post( $atts ) {
+	// If we didn't choose a post ID, return
+	if( ! $atts || ! $atts['post_id'] ) {
+		return 'No post ID specified.';
+	}
+
+	// Fetch current post object and set the global $post to point to it
+	$this_post = get_post( $atts['post_id'] );
+	global $post;
+	$post = $this_post;
+	setup_postdata( $post );
+
+	ob_start();
+		get_template_part( 'custom-templates/post-quickview' );
+	$output = ob_get_clean();
+
+	// Return $post to its original state
+	wp_reset_postdata();
+
+	return $output;
+}
+
+// PHP output buffering for the WordPress the_content filter hook
 add_filter( 'the_content', 'wpshout_add_single_post_footer' );
 function wpshout_add_single_post_footer( $content ) {
 	// Only on single Posts
@@ -25,29 +48,9 @@ function wpshout_add_single_post_footer( $content ) {
 	</div>
 
 	<?php
+	// Get the saved output as a string
 	$footer = ob_get_clean();
+
+	// Return the content plus the saved output
 	return $content . $footer;
-}
-
-add_shortcode( 'wpshout_quickview_post', 'wpshout_quickview_post' );
-function wpshout_quickview_post( $atts ) {
-	// If we didn't choose a post ID, return
-	if( ! $atts || ! $atts['post_id'] ) {
-		return 'No post ID specified.';
-	}
-
-	// Fetch current post object and set the global $post to point to it
-	$this_post = get_post( $atts['post_id'] );
-	global $post;
-	$post = $this_post;
-	setup_postdata( $post );
-
-	ob_start();
-		get_template_part( 'custom-templates/post-quickview' );
-	$output = ob_get_clean();
-
-	// Return $post to its original state
-	wp_reset_postdata();
-
-	return $output;
 }
